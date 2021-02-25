87ba1b771cab63c1071c8df483be7e48f12a15df
Stephan-Botes <stephan.botes1@gmail.com>
2021-02-25 17:25:37 +0200

    Add mssql functionality

--- src/main/js/comicScraper.js
+++ src/main/js/comicScraper.js
@@ -1,36 +1,53 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
 const potusParse = require('./potusParse');
-const url = 'http://www.coverbrowser.com/search?q=marvel';
+const url = 'http://www.coverbrowser.com/search?q=';
+const pageCount = 4;
+const sql = require('mssql')
 
-// This runs through the page's 30 comics and prints out the links to the images
-rp(url)
-    .then(function(html) {
-        //success!
-        const comicImages = [];
-        for (let i = 0; i < 30; i++) {
-            comicImages.push($('p > a > img', html)[i].attribs.src);
+run();
+
+async function run() {
+    await sql.connect('mssql://stephan:pass123@localhost/ComicStore');
+    const result = await sql.query(`SELECT Title AS Title, STRING_AGG(SeriesNumber,',') AS Series FROM Issues GROUP BY Title`);
+    // console.dir(result)
+
+    for (const row in result.recordset) {
+        const html = await rp(`${url}${row.Title}`)
+        console.log(parseHtml(html));
+    }
+    // const html = await rp(url);
+    //
+    // for (let i = 2; i <= pageCount; i++) {
+    //     console.log(i);
+    //     const newHtml = await rp(`${url}&page=${i}`);
+    //     parseHtml(newHtml)
+    // }
+}
+
+async function parseHtml(html) {
+    //success!
+    const comicImages = [];
+    const comicMap = new Map();
+    const imageCheck = $('p > a > img', html);
+
+    if (!imageCheck || !imageCheck[0]) {
+        return comicMap;
+    }
+
+    for (let i = 0; i < 30; i++) {
+        comicImages.push($('p > a > img', html)[i].attribs.src);
+    }
+
+    comicImages.forEach((link) => {
+        let name = link.substring(7, link.lastIndexOf("/"));
+        let issue = link.substring(link.lastIndexOf("/") + 1, link.length - 6);
+        let image = link;
+
+        if (!comicMap.has(issue)) {
+            comicMap.add(issue, `http://www.coverbrowser.com${image}`);
         }
-
-        comicImages.forEach((link) => {
-            let name = link.substring(7,link.lastIndexOf("/"));
-            let issue = link.substring(link.lastIndexOf("/") + 1, link.length -6);
-            let image = link;
-            console.log(`name: ${name} \n issue: #${issue} \n image: http://www.coverbrowser.com${image} \n`);
-        });
-
-        // return Promise.all(
-        //     comicImages.map(function(url) {
-        //         return comicParse('http://www.coverbrowser.com' + url);
-        //     })
-        // );
-    })
-
-    // .then(function(presidents) {
-    //     console.log(presidents);
-    // })
-
-    .catch(function(err) {
-        //handle error
-        console.log(err);
     });
+
+    return comicMap;
+}
